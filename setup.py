40c6f3079b87524cb48bdfa4248f31a34330a142
smbgray <smbgray@gmail.com>
2021-12-21 22:34:15 +0100

    basic structure for python project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='tbd name',
+    version='0.0.1',
+    description='TBD description',
+    author='smbgray',
+    author_email='smbgray@gmail.com',
+    packages=find_packages(exclude=["tests"]),
+)
